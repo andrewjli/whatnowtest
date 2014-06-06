6b782fbbb5bda361d1d7c9717267e52a87405b15
southrop <southrop113@gmail.com>
2014-06-06 02:26:22 +0100

    Debug data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from constants import SITENAME
-import urllib2, html2text
+import time, urllib2, html2text
 
 def configure_sitename(app):
     @app.context_processor
@@ -13,20 +13,32 @@ configure_sitename(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def search():
+    t0 = time.time()
+    t1 = time.time()
+    debug = 0
     data = {
         'url': '',
         'word': '',
-        'counter': 0
+        'counter': 0,
+        'time': time.time(),
+        'size': 0
     }
     counter = 0
     if request.method == 'GET':
         data['url'] = request.args.get('url', '')
         data['word'] = request.args.get('word', '')
+        if request.args.get('withdebug'):
+            debug = int(request.args.get('withdebug'))
     elif request.method == 'POST':
         data['url'] = request.form['url']
         data['word'] = request.form['word']
+        if request.form['withdebug']:
+            debug = int(request.form['withdebug'])
     else:
         return render_template('400.html')
+
+    if debug:
+        t0 = time.time()
 
     try:
         page = urllib2.urlopen(data['url'], timeout = 5)
@@ -41,7 +53,14 @@ def search():
     content = handler.handle(source.decode('utf8').encode('ascii','ignore'))
     data['counter'] = content.count(data['word'].encode('ascii','ignore'))
 
-    return render_template('index.html', data=data)
+    if debug:
+        t1 = time.time()-t0
+        data['time'] = t1
+        if page.info().getheader('Content-Length'):
+            data['size'] = int(page.info().getheader("Content-Length"))
+        return render_template('debug.html', data=data)
+    else:
+        return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
